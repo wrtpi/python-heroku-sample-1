67f4f5220f5708854c9262043301a00dca58973b
polaris <2186759+xuiv@users.noreply.github.com>
2021-07-03 20:25:44 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,66 +1,72 @@
 import os
 import time
+import string
+import random
+from tempfile import gettempdir
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.action_chains import ActionChains
 
-def isElementExist(client,element):
+def id_generator(size=15, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+def clicker(client):
     try:
-        client.find_element_by_xpath(element)
-        return True
+        while True:
+            print(len(client.window_handles))
+            if len(client.window_handles) < 1:
+                link = "window.open(\"http://adf.ly/25551289/www.pornhub.com/view_video.php?viewkey="+id_generator()+"\");"
+                print("--1-->> " + link)
+                client.execute_script(link)
+            client.switch_to.window(client.window_handles[0])
+            client.get("http://j.gs/25551289/girlxxx01")
+            time.sleep(5)
+            client.find_element_by_id("skip_bu2tton").click()
+            print(len(client.window_handles))                   
+            if len(client.window_handles) < 2:
+                link = "window.open(\"http://adf.ly/25551289/www.pornhub.com/view_video.php?viewkey="+id_generator()+"\");"
+                print("--2-->> " + link)
+                client.execute_script(link)
+            client.switch_to.window(client.window_handles[1])
+            client.get("http://j.gs/25551289/girlxxx02")
+            time.sleep(5)
+            client.find_element_by_id("skip_bu2tton").click()
+            print(len(client.window_handles))
+            if len(client.window_handles) < 3:
+                link = "window.open(\"http://adf.ly/25551289/www.pornhub.com/view_video.php?viewkey="+id_generator()+"\");"
+                print("--3-->> " + link)
+                client.execute_script(link)            
+            client.switch_to.window(client.window_handles[2])
+            client.get("http://j.gs/25551289/girlxxx03")
+            time.sleep(5)
+            client.find_element_by_id("skip_bu2tton").click()
+            print(len(client.window_handles))
+            if len(client.window_handles) < 4:
+                link = "window.open(\"http://adf.ly/25551289/www.pornhub.com/view_video.php?viewkey="+id_generator()+"\");"
+                print("--4-->> " + link)
+                client.execute_script(link)            
+            client.switch_to.window(client.window_handles[3])
+            client.get("http://j.gs/25551289/girlxxx04")
+            time.sleep(5)
+            client.find_element_by_id("skip_bu2tton").click()
+            time.sleep(10)
+            client.quit()
     except:
-        return False
-
-def waitElement(client,element):
-    while not isElementExist(client,element):
-        time.sleep(2)
-
-def loginc9():
-    try:
-        chrome_exec_shim = os.environ.get("GOOGLE_CHROME_SHIM", None)
-        chromedriver_path = os.environ.get("CHROMEDRIVER_PATH", None)
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.binary_location = chrome_exec_shim
-        client = webdriver.Chrome(chrome_options=chrome_options, executable_path=chromedriver_path)
-        client.set_window_size(1366, 768)
-        print("*********************************1*********************************")
-        client.get("https://c9.io/login")
-        waitElement(client,"//button[@type='submit']")
-        client.find_element_by_id("id-username").clear()
-        client.find_element_by_id("id-username").send_keys("user")
-        client.find_element_by_id("id-password").clear()
-        client.find_element_by_id("id-password").send_keys("password")
-        client.find_element_by_xpath("//button[@type='submit']").click()
-        print("*********************************2*********************************")
-        mainwindow = client.current_window_handle
-        client.execute_script('''window.open("about:blank", "_blank");''')
-        time.sleep(2)
-        client.switch_to_window(client.window_handles[1])
-        print("*********************************3*********************************")
-        client.get("https://codeanywhere.com/login")
-        waitElement(client,"//button[@type='submit']")
-        client.find_element_by_id("login-email").clear()
-        client.find_element_by_id("login-email").send_keys("user@gmail.com")
-        client.find_element_by_id("login-password").clear()
-        client.find_element_by_id("login-password").send_keys("password")
-        client.find_element_by_xpath("//button[@type='submit']").click()
-        print("*********************************4*********************************")
-        client.switch_to_window(mainwindow)    
-        waitElement(client,"//a[@href='https://ide.c9.io/user/app']")
-        client.find_element_by_xpath("//a[@href='https://ide.c9.io/user/app']").click()
-        time.sleep(1500)
-        client.get("https://appid.herokuapp.com")
-        time.sleep(300)
+        print("----exceptions----")
         client.quit()
-        return True
-    except:
-        return False
-        
-os.system("nohup gost -L socks+ws://:$PORT >/dev/null 2>&1 &")    
+        pass
 
 while True:
-    if not loginc9():
-        continue
-    time.sleep(600)
+    chrome_exec_shim = os.environ.get("GOOGLE_CHROME_SHIM", None)
+    chromedriver_path = os.environ.get("CHROMEDRIVER_PATH", None)
+    chrome_options.binary_location = chrome_exec_shim
+    chrome_options = webdriver.ChromeOptions()
+    chromeuser_path = os.path.join(gettempdir(), '.{}'.format(hash(os.times())))
+    chrome_options.add_argument('--user-data-dir=' + chromeuser_path)
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument("--disable-popup-blocking")
+    #chrome_options.add_argument('--proxy-server=socks5://localhost:1080')
+    #chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:5003")
+    prefs = {"profile.default_content_setting_values.notifications" : 2}
+    chrome_options.add_experimental_option("prefs", prefs)
+
+    client = webdriver.Chrome(chrome_options=chrome_options, executable_path=chromedriver_path)
+    clicker(client)      
