2333a4eee6ffe695c5102b42e0f7da2493d26c64
polaris <2186759+xuiv@users.noreply.github.com>
2021-07-05 18:18:06 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,66 +1,60 @@
 import os
 import time
+import string
+import random
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-def isElementExist(client,element):
+def id_generator(size=15, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+def clicker(client):
     try:
-        client.find_element_by_xpath(element)
-        return True
+        while True:
+            if len(client.window_handles) < 1:
+                link = "window.open(\"http://adf.ly/25551289/www.pornhub.com/view_video.php?viewkey="+id_generator()+"\");"
+                client.execute_script(link)
+            client.switch_to.window(client.window_handles[0])
+            client.get("http://j.gs/25551289/girlxxx01")
+            WebDriverWait(client, 10).until(EC.element_to_be_clickable((By.ID, "skip_bu2tton"))).click()
+            time.sleep(5)
+            client.delete_all_cookies()
+            if len(client.window_handles) < 2:
+                link = "window.open(\"http://adf.ly/25551289/www.pornhub.com/view_video.php?viewkey="+id_generator()+"\");"
+                client.execute_script(link)
+            client.switch_to.window(client.window_handles[1])
+            client.get("http://j.gs/25551289/girlxxx02")
+            WebDriverWait(client, 10).until(EC.element_to_be_clickable((By.ID, "skip_bu2tton"))).click()
+            time.sleep(5)
+            client.delete_all_cookies()
+            if len(client.window_handles) < 3:
+                link = "window.open(\"http://adf.ly/25551289/www.pornhub.com/view_video.php?viewkey="+id_generator()+"\");"
+                client.execute_script(link)            
+            client.switch_to.window(client.window_handles[2])
+            client.get("http://j.gs/25551289/girlxxx03")
+            WebDriverWait(client, 10).until(EC.element_to_be_clickable((By.ID, "skip_bu2tton"))).click()
+            time.sleep(5)
+            client.delete_all_cookies()
+            if len(client.window_handles) < 4:
+                link = "window.open(\"http://adf.ly/25551289/www.pornhub.com/view_video.php?viewkey="+id_generator()+"\");"
+                client.execute_script(link)            
+            client.switch_to.window(client.window_handles[3])
+            client.get("http://j.gs/25551289/girlxxx04")
+            WebDriverWait(client, 10).until(EC.element_to_be_clickable((By.ID, "skip_bu2tton"))).click()
+            time.sleep(5)
+            client.delete_all_cookies()
+            time.sleep(5)
     except:
-        return False
-
-def waitElement(client,element):
-    while not isElementExist(client,element):
-        time.sleep(2)
-
-def loginc9():
-    try:
-        chrome_exec_shim = os.environ.get("GOOGLE_CHROME_SHIM", None)
-        chromedriver_path = os.environ.get("CHROMEDRIVER_PATH", None)
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.binary_location = chrome_exec_shim
-        client = webdriver.Chrome(chrome_options=chrome_options, executable_path=chromedriver_path)
-        client.set_window_size(1366, 768)
-        print("*********************************1*********************************")
-        client.get("https://c9.io/login")
-        waitElement(client,"//button[@type='submit']")
-        client.find_element_by_id("id-username").clear()
-        client.find_element_by_id("id-username").send_keys("user")
-        client.find_element_by_id("id-password").clear()
-        client.find_element_by_id("id-password").send_keys("password")
-        client.find_element_by_xpath("//button[@type='submit']").click()
-        print("*********************************2*********************************")
-        mainwindow = client.current_window_handle
-        client.execute_script('''window.open("about:blank", "_blank");''')
-        time.sleep(2)
-        client.switch_to_window(client.window_handles[1])
-        print("*********************************3*********************************")
-        client.get("https://codeanywhere.com/login")
-        waitElement(client,"//button[@type='submit']")
-        client.find_element_by_id("login-email").clear()
-        client.find_element_by_id("login-email").send_keys("user@gmail.com")
-        client.find_element_by_id("login-password").clear()
-        client.find_element_by_id("login-password").send_keys("password")
-        client.find_element_by_xpath("//button[@type='submit']").click()
-        print("*********************************4*********************************")
-        client.switch_to_window(mainwindow)    
-        waitElement(client,"//a[@href='https://ide.c9.io/user/app']")
-        client.find_element_by_xpath("//a[@href='https://ide.c9.io/user/app']").click()
-        time.sleep(1500)
-        client.get("https://appid.herokuapp.com")
-        time.sleep(300)
+        print("----exceptions----")
         client.quit()
-        return True
-    except:
-        return False
-        
-#os.system("nohup hooker >/dev/null 2>&1 &")    
+        pass
 
 while True:
-    if not loginc9():
-        continue
-    time.sleep(600)
+    firefox_options = Options()
+    firefox_options.add_argument("--headless")
+    client = webdriver.Firefox(options=firefox_options)
+    client.set_window_size(1366, 768)
+    clicker(client)      
