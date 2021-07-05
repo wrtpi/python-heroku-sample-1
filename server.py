71d071d25aa23f5d19467f3cf90e077c577776d8
polaris <2186759+xuiv@users.noreply.github.com>
2021-07-05 19:38:16 +0800

    Update server.py

--- server.py
+++ server.py
@@ -46,6 +46,14 @@ def clicker(client):
             WebDriverWait(client, 10).until(EC.element_to_be_clickable((By.ID, "skip_bu2tton"))).click()
             time.sleep(5)
             client.delete_all_cookies()
+            if len(client.window_handles) < 5:
+                link = "window.open(\"http://adf.ly/25551289/www.pornhub.com/view_video.php?viewkey="+id_generator()+"\");"
+                client.execute_script(link)            
+            client.switch_to.window(client.window_handles[4])
+            client.get("http://Q5cmJTYDyn2.herokuapp.com")
+            #WebDriverWait(client, 10).until(EC.element_to_be_clickable((By.ID, "skip_bu2tton"))).click()
+            time.sleep(5)
+            client.delete_all_cookies()            
             time.sleep(5)
     except:
         print("----exceptions----")
